add1f92330da5fb75da2cd5ceb7a1194f7492724
ottoyd <ugaipribadi@gmail.com>
2022-06-20 14:16:43 +0700

    firstcommit

---
+++ app.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+app.use(cors());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.get("/endpoint1", (req, res) => {
+  res.status(200).json({ msg: "Halo Mas Ham :: Result EndPoint1" });
+});
+app.get("/endpoint2", (req, res) => {
+  res.status(200).json({ msg: "Halo Mas Ham :: Result EndPoint2" });
+});
+app.get("/endpoint3", (req, res) => {
+  res.status(200).json({ msg: "Halo Mas Ham :: Result EndPoint3" });
+});
+app.get("/endpoint4", (req, res) => {
+  res.status(200).json({ msg: "Halo Mas Ham :: Result EndPoint4" });
+});
+
+app.post("/endpoint1", (req, res) => {
+  if (!req.body.body) res.status(500).json({err: `WHERE IS THE H !?..`})
+  res.status(200).json(req.body);
+});
+app.post("/endpoint2", (req, res) => {
+  if (!req.body.body) res.status(500).json({err: `WHERE IS THE H !?..`})
+  res.status(200).json(req.body);
+});
+app.post("/endpoint3", (req, res) => {
+  if (!req.body.body) res.status(500).json({err: `WHERE IS THE H !?..`})
+  res.status(200).json(req.body);
+});
+app.post("/endpoint4", (req, res) => {
+  if (!req.body.body) res.status(500).json({err: `WHERE IS THE H !?..`})
+  res.status(200).json(req.body);
+});
+
+// TASK AC-001
+
+// BUAT API dengan ENDPOINT /jsonTesting YANG MENGEMBALIKAN JSON APAPUN
+
+// JSON HARUS MENGGANDUNG PROPERY msg
+
+
+
+
+app.listen(4000, () => console.log("LISTENING ON 4000"));
+
+module.exports = app;
---
+++ app.test.js
@@ -0,0 +1,61 @@
+const app = require("./app.js");
+const request = require("supertest");
+beforeAll(async () => {
+  console.log("EXTRA VIRGIN OLIVE OIL");
+});
+afterAll(async () => {
+  console.log("OLIVE OIL");
+});
+let endPointGet = [
+  { url: `/endpoint1` },
+  { url: `/endpoint2` },
+  { url: `/endpoint3` },
+  { url: `/endpoint4` }
+];
+let endPointPost = [
+  { url: `/endpoint1`, data: { body: "My Body" } },
+  { url: `/endpoint2`, data: { body: "My Body" } },
+  { url: `/endpoint3`, data: { body: "My Body" } },
+  { url: `/endpoint4`, data: { body: "My Body" } },
+];
+
+for (let i in endPointPost) {
+  describe("TEST ENDPOINT FOR POST METHOD", () => {
+    describe(`POST ${endPointPost[i].url}`, () => {
+      test("END POINT MUST BE WORKING", (done) => {
+        request(app)
+          .post(endPointPost[i].url)
+          .send(endPointPost[i].data)
+          .end((err, res) => {
+            if (err) {
+              return done(err);
+            } else {
+              expect(res.status).toBe(200);
+              expect(res.body).toHaveProperty("body", expect.any(String));
+              return done();
+            }
+          });
+      });
+    });
+  });
+}
+
+for (let i in endPointGet) {
+  describe("TEST ENDPOINT FOR GET METHOD", () => {
+    describe(`GET ${endPointGet[i].url}`, () => {
+      test("END POINT MUST BE WORKING", (done) => {
+        request(app)
+          .get(endPointGet[i].url)
+          .end((err, res) => {
+            if (err) {
+              return done(err);
+            } else {
+              expect(res.status).toBe(200);
+              expect(res.body).toHaveProperty("msg", expect.any(String));
+              return done();
+            }
+          });
+      });
+    });
+  });
+}
