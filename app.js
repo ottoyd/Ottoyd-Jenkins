0a37725ec4a4767144b7993cc0313cf2ac6af1b3
Ottoyd <39210009+ottoyd@users.noreply.github.com>
2024-07-02 14:13:47 +0700

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-console.log('flyyyy my steeed')
+console.log('flyyy my steeed')
 
 function doShit (duration) {
   const start = Date.now()
