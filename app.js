3bb32c6f5584b8eeac70773898c3afdc8fd5c591
ottoyd <ugaipribadi@gmail.com>
2024-07-02 14:33:54 +0700

    trigger

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-console.log('flyyy my sted')
+console.log('fly my sted')
 
 function doShit (duration) {
   const start = Date.now()
