fc52f9141107225c11908b5be7c34dcadcb1be86
ottoyd <ugaipribadi@gmail.com>
2024-07-02 15:10:35 +0700

    fix Error

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.get('/slow', (req, res) => {
 })
 
 app.get("/endpoint1", (req, res) => {
-  res.status(200).json({ msgs: "Halo Mas Ham :: Result EndPoint1" });
+  res.status(200).json({ msg: "Halo Mas Ham :: Result EndPoint1" });
 });
 app.get("/endpoint2", (req, res) => {
   res.status(200).json({ msg: "Halo Mas Ham :: Result EndPoint2" });
